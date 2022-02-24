e7aba447f8475ae1685cbf18bcb4c72ec160dbdc
Rin-I <inagaigai@gmail.com>
2022-02-25 08:45:53 +0900

    TailwindCSSに書き換えました

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,13 @@
 import React, { useState, useEffect } from "react"
 import "./App.css"
-import { Container, AppBar, Typography, Grow, Grid } from "@mui/material"
 import icon from "./images/icon.png"
 import Posts from "./Posts/Posts"
 import Form from "./Form/Form"
-import { makeStyles } from "@mui/styles"
 import { useAppDispatch } from "./app/hooks"
 import { FetchAllPosts } from "./features/posts/postSlice"
 
-const useStyles = makeStyles({
-  appBar: {
-    borderRadius: 15,
-    margin: "30px 0",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  heading: {
-    color: "rgba(0,183,255, 1)",
-  },
-  caution: {
-    color: "#000",
-    fontSize: "1rem",
-  },
-  image: {
-    marginLeft: "15px",
-  },
-})
-
 const App: React.FC = () => {
   const [currentId, setCurrentId] = useState("")
-  const classes = useStyles()
   const dispatch = useAppDispatch()
 
   useEffect(() => {
@@ -48,18 +24,16 @@ const App: React.FC = () => {
         </h1>
         <img className="h-12" src={icon} alt="memories" height="60" />
       </header>
-      <Grow in>
-        <div className="p-5">
-          <div className="grid grid-cols-2">
-            <div className="col-start-1 col-end-3">
-              <Posts setCurrentId={setCurrentId} />
-            </div>
-            <div className="">
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </div>
+      <div className="p-5">
+        <div className="flex justify-between px-10">
+          <div className="w-3/4">
+            <Posts setCurrentId={setCurrentId} />
+          </div>
+          <div className="w-1/4">
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
           </div>
         </div>
-      </Grow>
+      </div>
     </div>
   )
 }
--- client/src/Form/Form.tsx
+++ client/src/Form/Form.tsx
@@ -36,7 +36,7 @@ type PROPS = {
 }
 
 const Form: React.FC<PROPS> = ({ currentId, setCurrentId }) => {
-  const classes = useStyles()
+  const classNamees = useStyles()
   const dispatch = useAppDispatch()
   const post = useAppSelector((store) =>
     currentId ? store.posts.find((p) => p._id === currentId) : null
@@ -77,87 +77,189 @@ const Form: React.FC<PROPS> = ({ currentId, setCurrentId }) => {
   }
 
   return (
-    <Paper className={classes.paper}>
-      <form
-        autoComplete="off"
-        noValidate
-        className={classes.form}
-        onSubmit={handleSubmit}
-      >
-        <Typography variant="h6">
-          家計簿を{currentId ? "編集する" : "追加する"}
-        </Typography>
-        <TextField
+    <div className="register bg-white rounded-lg p-3">
+      <h2 className="text-2xl mb-6">
+        家計簿を{currentId ? "編集する" : "追加する"}
+      </h2>
+      <div className="display_name flex border rounded text-gray-500 mb-4">
+        <input
+          className="outline-none px-4 h-full py-2 text-lg"
+          type="text"
+          placeholder="投稿者"
           name="creator"
-          variant="outlined"
-          label="Creator"
-          fullWidth
           value={postData.creator}
           onChange={(
             e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
           ) => setPostData({ ...postData, creator: e.target.value })}
         />
-        <TextField
+      </div>
+
+      <div className="username flex border rounded text-gray-500 mb-4">
+        <input
+          className="outline-none px-4 h-full py-2 text-lg"
+          type="text"
+          placeholder="カテゴリ"
           name="title"
-          variant="outlined"
-          label="Title"
-          fullWidth
           value={postData.title}
           onChange={(
             e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
           ) => setPostData({ ...postData, title: e.target.value })}
         />
-        <TextField
+      </div>
+
+      <div className="password flex border rounded text-gray-500 mb-4">
+        <input
+          className="outline-none px-4 h-full py-2 text-lg"
+          type="password"
+          placeholder="料金"
           name="message"
-          variant="outlined"
-          label="Message"
-          fullWidth
           value={postData.message}
           onChange={(
             e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
           ) => setPostData({ ...postData, message: e.target.value })}
         />
-        <TextField
+      </div>
+
+      <div className="password flex border rounded text-gray-500 mb-4">
+        <input
+          className="outline-none px-4 h-full py-2 text-lg"
+          type="password"
+          placeholder="分類"
           name="tags"
-          variant="outlined"
-          label="Tags"
-          fullWidth
           value={postData.tags}
           onChange={(
             e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
           ) => setPostData({ ...postData, tags: e.target.value })}
         />
-        {/* <div className={classes.fileInput}>
-        <FileBase
-          type="file"
-          multiple={false}
-          onDone={({ base64: any }) =>
-            setPostData({ ...postData, selectedFile: base64 })
-          }
-        />
-      </div> */}
-        <Button
-          className={classes.buttonSubmit}
-          variant="contained"
-          color="primary"
-          size="large"
-          type="submit"
-          fullWidth
-          onClick={handleSubmit}
-        >
-          Submit
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          size="small"
-          onClick={clear}
-          fullWidth
-        >
-          Clear
-        </Button>
-      </form>
-    </Paper>
+      </div>
+      <div className="submit border rounded mb-4 bg-blue-600 text-white cursor-pointer">
+        <div className="wrapper flex w-max mx-auto">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-5 my-auto"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+            />
+          </svg>
+          <input
+            className="outline-none px-2 h-full cursor-pointer py-2 text-lg bg-transparent"
+            type="button"
+            value="新規追加"
+          />
+        </div>
+      </div>
+      <div className="submit border rounded mb-4 bg-red-400 text-white cursor-pointer">
+        <div className="wrapper flex w-max mx-auto">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-5 my-auto"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+            />
+          </svg>
+          <input
+            className="outline-none px-2 h-full cursor-pointer py-2 text-lg bg-transparent"
+            type="button"
+            value="Clear"
+          />
+        </div>
+      </div>
+    </div>
+    // <Paper classNameName={classNamees.paper}>
+    //   <form
+    //     autoComplete="off"
+    //     noValidate
+    //     classNameName={classNamees.form}
+    //     onSubmit={handleSubmit}
+    //   >
+    //     <Typography variant="h6">
+    //       家計簿を{currentId ? "編集する" : "追加する"}
+    //     </Typography>
+    //     <TextField
+    //       name="creator"
+    //       variant="outlined"
+    //       label="Creator"
+    //       fullWidth
+    //       value={postData.creator}
+    //       onChange={(
+    //         e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+    //       ) => setPostData({ ...postData, creator: e.target.value })}
+    //     />
+    //     <TextField
+    //       name="title"
+    //       variant="outlined"
+    //       label="Title"
+    //       fullWidth
+    //       value={postData.title}
+    //       onChange={(
+    //         e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+    //       ) => setPostData({ ...postData, title: e.target.value })}
+    //     />
+    //     <TextField
+    //       name="message"
+    //       variant="outlined"
+    //       label="Message"
+    //       fullWidth
+    //       value={postData.message}
+    //       onChange={(
+    //         e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+    //       ) => setPostData({ ...postData, message: e.target.value })}
+    //     />
+    //     <TextField
+    //       name="tags"
+    //       variant="outlined"
+    //       label="Tags"
+    //       fullWidth
+    //       value={postData.tags}
+    //       onChange={(
+    //         e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+    //       ) => setPostData({ ...postData, tags: e.target.value })}
+    //     />
+    //     {/* <div classNameName={classNamees.fileInput}>
+    //     <FileBase
+    //       type="file"
+    //       multiple={false}
+    //       onDone={({ base64: any }) =>
+    //         setPostData({ ...postData, selectedFile: base64 })
+    //       }
+    //     />
+    //   </div> */}
+    //     <Button
+    //       classNameName={classNamees.buttonSubmit}
+    //       variant="contained"
+    //       color="primary"
+    //       size="large"
+    //       type="submit"
+    //       fullWidth
+    //       onClick={handleSubmit}
+    //     >
+    //       Submit
+    //     </Button>
+    //     <Button
+    //       variant="contained"
+    //       color="secondary"
+    //       size="small"
+    //       onClick={clear}
+    //       fullWidth
+    //     >
+    //       Clear
+    //     </Button>
+    //   </form>
+    // </Paper>
   )
 }
 
